f719da8afa8140665b62eb484a24bcba5e7a1721
Jimin Hong <jhong.dev@gmail.com>
2016-01-30 13:38:57 -0500

    added mongodb uri

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  db: 'mongodb://jhong:jhong@ds063124.mongolab.com:63124/heroku_cvn5n8bf',
+
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
